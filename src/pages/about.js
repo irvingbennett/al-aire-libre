4b1d096252ebb64e92bf428bc7dc2ce5d974744f
Irving Bennett <irving@alairelibre.net>
2021-06-07 18:55:48 -0500

    Step #2 done.

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import * as React from 'react'
+import { Link } from 'gatsby'
+import Layout from '../components/layout'
+
+const AboutPage = () => {
+    return (
+        <Layout pageTitle="About Page">
+            <p>Hi there! This is my about page.</p>
+        </Layout>
+    )
+}
+
+export default AboutPage
